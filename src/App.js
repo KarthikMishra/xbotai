32c78921ca24815c35f7dfee16a1a590771724ae
KarthikMishra <karthik.mishra@gmail.com>
2025-05-26 21:00:47 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,91 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-import ChatInterface from "./Components/ChatInterface";
-import Sidebar from "./Components/Sidebar";
-import {createTheme, ThemeProvider} from "@mui/material/styles";
-import {Grid as Grid} from "@mui/material";
-import Conversations from "./Components/Conversations";
-import React, {useEffect, useState} from "react";
-import data from "./sampleData.json";
-import Navbar from "./Components/Navbar";
-
-const theme = createTheme({
-	palette: {
-		primary: {
-			main: '#AF9FCD',
-			light: '#F9FAFA',
-			dark: '#9785BA',
-		},
-		secondary: {
-			main: '#D7C7F4',
-			light: '#FFFFFF',
-			dark: '',
-		}
-	}
-})
+import { Outlet } from 'react-router-dom'
+import { ThemeContext } from './theme/ThemeContext';
+import { useState, useEffect } from 'react';
+import Sidebar from './components/Sidebar/Sidebar';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { getThemePallete } from './theme/ThemePallete';
+import { Grid } from '@mui/material'
 
 function App() {
-	const [aiData, setAiData] = useState([]);
-	const [conversations, setConversations] = useState([]);
-	const [showPastConversations, setShowPastConversations] = useState(false);
-	const [messages, setMessages] = useState([]);
-	const [input, setInput] = useState('');
-	const [response, setResponse] = useState('');
 
-	const updateMessages = (newMessage) => {
-		setMessages(prevMessages => [...prevMessages, newMessage]);
-	}
+  const [mode, setMode] = useState(localStorage.getItem('theme') || 'light')
+  const [chat, setChat] = useState([])
+  const [menuOpen, setMenuOpen] = useState(false)
 
-	const updateInput = (value) => {
-		setInput(value);
-	}
+  //create theme
+  const theme = React.useMemo(() => createTheme(getThemePallete(mode)), [mode]);
 
-	useEffect( () => {
-		setAiData(data);
-	}, []);
+  //save theme mode in localstorage
+  useEffect(() => {
 
-	useEffect(() => {
-		let res = aiData.filter((x)=>x.question.includes(input));
-		if(res.length === 0){setResponse("As a AI language model, I cannot help you out!")}
-		else {setResponse(res[0].response);}
-	}, [input]);
+    localStorage.setItem('theme', mode)
 
-	const updateConversations = () => {
-		setConversations(prevConv => [...prevConv, messages]);
-	}
+  }, [mode])
 
-	const updateShowPastConversations = () => {
-		setShowPastConversations(true);
-	}
+  return (
+    <ThemeContext.Provider value={{ mode: mode, setMode: setMode }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
 
-	const handleClickNewChat = () => {
-		setShowPastConversations(false);
-		setMessages([]);
-	}
+        <Grid container sx={{ background: 'linear-gradient(rgba(215, 199, 244, 0.2), rgba(151, 133, 186, 0.2))' }} >
+          <Grid
+            item xs={12}
+            md={2.5}
+            sx={{
+              bgcolor: 'primary.light',
+              '@media (max-width:800px)': {
+                width: '70%',
+                transform: menuOpen ? 'translateX(0)' : 'translateX(-100%)',
+                transition: 'transform 400ms ease',
+              },
+            }}
+            position={{ xs: 'fixed', md: 'relative' }}
+            height={'100vh'}
+            zIndex={{ xs: 9999, md: 1 }}
+            boxShadow={{ xs: menuOpen ? 10 : 0, md: 0 }}
+          >
+            <Sidebar setChat={setChat} closeMenu={() => setMenuOpen(false)} />
+          </Grid>
+          <Grid item xs={12} md={9.5}>
+            <Outlet context={{ chat: chat, setChat: setChat, handleMobileMenu: setMenuOpen }} />
+          </Grid>
+        </Grid>
 
-	const handleSaveClick = () => {
-		updateConversations();
-	};
-
-	return (
-		<ThemeProvider theme={theme}>
-			<div className="App">
-				<Grid container spacing={1} sx={{backgroundColor: '#D7C7F433'}}>
-					<Grid size={{xs:1, lg:2, xl:3}} sx={{backgroundColor: 'white'}}>
-						<Sidebar handleClickNewChat={handleClickNewChat} handleClickPastConversations={updateShowPastConversations} />
-					</Grid>
-					<Grid size={{xs:'grow', lg:'grow', xl:'grow'}}>
-						<Navbar />
-						{showPastConversations===true ? (
-							<Conversations conversations={conversations}/>
-						) : (
-							<ChatInterface messages={messages} updateMessages={updateMessages} handleSaveClick={handleSaveClick} input={input} updateInput={updateInput} response={response}/>
-						)}
-					</Grid>
-				</Grid>
-			</div>
-		</ThemeProvider>
-	);
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
 }
 
 export default App;
--- src/Components/ChatCard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Stack} from "@mui/material";
-
-export default function ChatCard(props) {
-	return (
-		null
-	);
-}
