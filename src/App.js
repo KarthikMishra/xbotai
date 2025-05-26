1cdcddf0e041d466fb75dd3f603a5e3e4c08091a
KarthikMishra <karthik.mishra@gmail.com>
2025-05-26 18:36:27 +0530

    Updated interface

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
 import logo from './logo.svg';
 import './App.css';
+import ChatInterface from "./Components/ChatInterface";
+import Sidebar from "./Components/Sidebar";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import {Grid as Grid} from "@mui/material";
+import Conversations from "./Components/Conversations";
+import React, {useEffect, useState} from "react";
+import data from "./sampleData.json";
+import Navbar from "./Components/Navbar";
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#AF9FCD',
+			light: '#F9FAFA',
+			dark: '#9785BA',
+		},
+		secondary: {
+			main: '#D7C7F4',
+			light: '#FFFFFF',
+			dark: '',
+		}
+	}
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [aiData, setAiData] = useState([]);
+	const [conversations, setConversations] = useState([]);
+	const [showPastConversations, setShowPastConversations] = useState(false);
+	const [messages, setMessages] = useState([]);
+	const [input, setInput] = useState('');
+	const [response, setResponse] = useState('');
+
+	const updateMessages = (newMessage) => {
+		setMessages(prevMessages => [...prevMessages, newMessage]);
+	}
+
+	const updateInput = (value) => {
+		setInput(value);
+	}
+
+	useEffect( () => {
+		setAiData(data);
+	}, []);
+
+	useEffect(() => {
+		let res = aiData.filter((x)=>x.question.includes(input));
+		if(res.length === 0){setResponse("As a AI language model, I cannot help you out!")}
+		else {setResponse(res[0].response);}
+	}, [input]);
+
+	const updateConversations = () => {
+		setConversations(prevConv => [...prevConv, messages]);
+	}
+
+	const updateShowPastConversations = () => {
+		setShowPastConversations(true);
+	}
+
+	const handleClickNewChat = () => {
+		setShowPastConversations(false);
+		setMessages([]);
+	}
+
+	const handleSaveClick = () => {
+		updateConversations();
+	};
+
+	return (
+		<ThemeProvider theme={theme}>
+			<div className="App">
+				<Grid container spacing={1} sx={{backgroundColor: '#D7C7F433'}}>
+					<Grid size={{xs:1, lg:2, xl:3}} sx={{backgroundColor: 'white'}}>
+						<Sidebar handleClickNewChat={handleClickNewChat} handleClickPastConversations={updateShowPastConversations} />
+					</Grid>
+					<Grid size={{xs:'grow', lg:'grow', xl:'grow'}}>
+						<Navbar />
+						{showPastConversations===true ? (
+							<Conversations conversations={conversations}/>
+						) : (
+							<ChatInterface messages={messages} updateMessages={updateMessages} handleSaveClick={handleSaveClick} input={input} updateInput={updateInput} response={response}/>
+						)}
+					</Grid>
+				</Grid>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
